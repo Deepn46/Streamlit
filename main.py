05e3998b62580df8364b107734443c946198f030
Deepn46 <deepn46@gmail.com>
2022-07-02 09:47:04 +0530

    Demo page updated
    Demo page updated

--- main.py
+++ main.py
@@ -14,4 +14,16 @@ df=pd.read_csv(url)
 
 df1=df.melt(id_vars=['Country/Region','Province/State','Lat','Long'],var_name='Date',value_name='Running total')  
 
+page_selected=st.sidebar.radio('Select Page',['Demo','Cases','Deaths','Recovered'])
+
+if page_selected=='Demo':
+    st.header('Covid19')
+    st.text('Coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus.')
+    st.subheader('Daily cases')
+    selected_country=st.selectbox('Select Country',list(df1['Country/Region'].unique()))
+    st.write('Total cases till today in ',selected_country,' is')
+    df1[df1['Country/Region']=='India'][['Date','Running total']].tail(1)
+    st.subheader(int(df1[df1['Country/Region']=='India']['Running total'].tail(1)))
+    st.table(df1[df1['Country/Region']==selected_country].tail(1).reset_index())
+    
 
